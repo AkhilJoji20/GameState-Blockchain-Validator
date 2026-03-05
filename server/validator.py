2acf0114d0de998e8cc7464ccb49eee047ffd9e7
Akhil J <akhiljoji20@gmail.com>
2026-03-05 22:05:56 +0530

    Update validator.py

--- server/validator.py
+++ server/validator.py
@@ -1,11 +1,10 @@
 def validate_block(prev_state, new_state):
 
-    # detect level jump
     if new_state["level"] - prev_state["level"] > 1:
         return False, "Invalid level jump detected"
 
-    # detect suspicious money gain
     if new_state["money"] - prev_state["money"] > 5000:
         return False, "Suspicious money gain detected"
+
 
     return True, "Valid"
